b90f755f35d43fc048765992cbe582ec3d37cfca
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-02-21 19:54:21 +0100

    Add comments

--- src/main/java/com/myorg/HomeController.java
+++ src/main/java/com/myorg/HomeController.java
@@ -3,9 +3,15 @@ package com.myorg;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RestController for home
+ **/
 @RestController
 public class HomeController {
 
+	/**
+	* Request home path
+	**/
     @RequestMapping(path = "/")
     public String home() {
         return "Hello REST Microservice World";
